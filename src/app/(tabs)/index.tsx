2c19fd7b22f322a30ba30afa1bf33e377040ea80
Walid Esmael <walidesmael32@gmail.com>
2024-07-09 14:06:46 -0500

    Updated HomeScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { ScrollView, StyleSheet, Image } from 'react-native';
 import { Text, View } from '@/src/components/Themed';
 import React from 'react';
+import { blue } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 export default function HomeScreen() {
   return (
@@ -10,7 +11,7 @@ export default function HomeScreen() {
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <Image
         style={styles.image}
-        source={require('@/src/images/Soccer.png')}
+        source={require('@/src/images/soccernew.jpg')}
         />
         <Text style={styles.description}>
           Welcome to "Run It" – the ultimate app for organizing and managing pickup soccer games with ease! 
@@ -18,6 +19,10 @@ export default function HomeScreen() {
           you need to enjoy the beautiful game of soccer.
           Get started today and never miss a chance to play!
         </Text>
+        <Image
+        style={styles.image}
+        source={require('@/src/images/Soccer.png')}
+        />
       </View>
     </ScrollView>
   );
@@ -31,25 +36,33 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ADD8E6'
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
+    marginVertical: 1,
+    height: 0,
     width: '80%',
   },
   description: {
     fontSize: 16,
     textAlign: 'center',
     marginHorizontal: 20,
+    fontFamily: 'Helvetica, Arial, sans-serif',
+    
+
+
   },
   image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
+    width: '100%',
+    height: 250,
+    marginTop: 0,
+    marginBottom: 15,
   },
+
 });
 
