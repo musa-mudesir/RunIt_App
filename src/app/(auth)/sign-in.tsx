abd73c9e2e46906346d982cc86e12c8fd5acd309
Musa Mudesir <musamudesir@Musas-Air.lan>
2024-07-14 04:40:21 -0500

    Finished firebase config, added login feature

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,14 +1,30 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+// sign-in.tsx
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
 import React, { useState } from 'react';
 import Button from '../../components/Button';
 import Colors from '../../constants/Colors';
 import { Link, Stack } from 'expo-router';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-// import app from '../firebaseconfig'
+import { app } from '../firebaseconfig'; // Ensure this path is correct
+import { useNavigation } from '@react-navigation/native';
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const auth = getAuth(app); // Initialize auth here
+  const navigation = useNavigation();
+
+  const handleSignIn = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log('User signed in successfully:', userCredential.user);
+      navigation.navigate('(tabs)');
+      // Navigate to the home screen on successful sign-in
+    } catch (error) {
+      console.error('Error signing in:', error);
+      Alert.alert('Sign in error', 'Please enter the correct email and password', [{text: 'OK' }]);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -31,9 +47,7 @@ const SignInScreen = () => {
         secureTextEntry
       />
 
-      <Link href={'/(tabs)'} asChild>
-      <Button text="Sign in" />
-      </Link>
+      <Button text="Sign in" onPress={handleSignIn} />
       <Link href="/sign-up" style={styles.textButton}>
         Create an account
       </Link>
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -3,10 +3,22 @@ import React, { useState } from 'react';
 import Button from '../../components/Button';
 import Colors from '../../constants/Colors';
 import { Link, Stack } from 'expo-router';
+import { auth } from '../firebaseconfig'; // Ensure the path is correct
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const SignUpScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log('User signed up successfully');
+      // Navigate to the home screen on successful sign-up
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -29,7 +41,7 @@ const SignUpScreen = () => {
         secureTextEntry
       />
 
-      <Button text="Create account" />
+      <Button text="Create account" onPress={handleSignUp} />
       <Link href="/sign-in" style={styles.textButton}>
         Sign in
       </Link>
